37400007dd4af9dd184268200c561c6b9b3336e0
Dmitriy Mishanin <dmitriymishanin@gmail.com>
2021-09-23 17:04:24 +0300

    Update settings

--- monstability/monstability/settings.py
+++ monstability/monstability/settings.py
@@ -94,30 +94,30 @@ TEMPLATES = [
 WSGI_APPLICATION = 'monstability.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('MSDBNAME'),
-#         'USER': os.environ.get("MSDBUSER"),
-#         'PASSWORD': os.environ.get("MSDBPASSWORD"),
-#         'HOST': os.environ.get("MSDBHOST"),
-#         'PORT': os.environ.get("MSDBPORT"),
-#     }
-# }
+#Database
+#https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'msdb',
-        'USER': 'mseuser',
-        'PASSWORD': 'P@ssw0rd',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.environ.get('MSDBNAME'),
+        'USER': os.environ.get("MSDBUSER"),
+        'PASSWORD': os.environ.get("MSDBPASSWORD"),
+        'HOST': os.environ.get("MSDBHOST"),
+        'PORT': os.environ.get("MSDBPORT"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'msdb',
+#         'USER': 'mseuser',
+#         'PASSWORD': 'P@ssw0rd',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
